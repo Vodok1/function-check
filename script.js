32a5604e11f7df1d957690e7c8c2a9d5db9a9d69
Jakub Bauza <bulzor95@gmail.com>
2020-09-03 23:09:11 +0200

    test

--- script.js
+++ script.js
@@ -331,4 +331,113 @@ class ValidatorRegon {
     }
   }
 }
-console.log(ValidatorRegon.valRegon("302690873"));
+//console.log(ValidatorRegon.valRegon("302690873"));
+
+const time = "PT5M6S";
+function convertTime(duration) {
+  var a = duration.match(/\d+/g);
+
+  if (
+    duration.indexOf("M") >= 0 &&
+    duration.indexOf("H") == -1 &&
+    duration.indexOf("S") == -1
+  ) {
+    a = [0, a[0], 0];
+  }
+
+  if (duration.indexOf("H") >= 0 && duration.indexOf("M") == -1) {
+    a = [a[0], 0, a[1]];
+  }
+  if (
+    duration.indexOf("H") >= 0 &&
+    duration.indexOf("M") == -1 &&
+    duration.indexOf("S") == -1
+  ) {
+    a = [a[0], 0, 0];
+  }
+
+  duration = 0;
+
+  if (a.length == 3) {
+    duration = duration + parseInt(a[0]) * 3600;
+    duration = duration + parseInt(a[1]) * 60;
+    duration = duration + parseInt(a[2]);
+  }
+
+  if (a.length == 2) {
+    duration = duration + parseInt(a[0]) * 60;
+    duration = duration + parseInt(a[1]);
+  }
+
+  if (a.length == 1) {
+    duration = duration + parseInt(a[0]);
+  }
+  var h = Math.floor(duration / 3600);
+  var m = Math.floor((duration % 3600) / 60);
+  var s = Math.floor((duration % 3600) % 60);
+  return duration * 1000;
+}
+//////////////////////////////////////
+// Setup
+var collection = {
+  2548: {
+    album: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    album: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    album: "ABBA Gold",
+  },
+};
+// Only change code below this line
+// Setup
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Kristian", "lastName"));
